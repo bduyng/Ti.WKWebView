31fb8cc0c860adef900ad9c499ddef744eae0ef2
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-04-17 12:42:57 +0200

    Move configuration to own API

---
+++ ios/Classes/TiWkwebviewConfigurationProxy.h
@@ -0,0 +1,27 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2017 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+#import "TiProxy.h"
+
+@interface TiWkwebviewConfigurationProxy : TiProxy
+
+- (WKWebViewConfiguration *)configuration;
+
+- (void)setPreferences:(id)args;
+
+- (void)setSelectionGranularity:(id)value;
+
+- (void)setMediaTypesRequiringUserActionForPlayback:(id)value;
+
+- (void)setSuppressesIncrementalRendering:(id)value;
+
+- (void)setAllowsInlineMediaPlayback:(id)value;
+
+- (void)setAllowsAirPlayMediaPlayback:(id)value;
+
+- (void)setAllowsPictureInPictureMediaPlayback:(id)value;
+
+@end
---
+++ ios/Classes/TiWkwebviewConfigurationProxy.m
@@ -0,0 +1,89 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2017 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+#import <WebKit/WebKit.h>
+
+#import "TiWkwebviewConfigurationProxy.h"
+#import "TiWkwebviewProcessPoolProxy.h"
+#import "TiUtils.h"
+
+@implementation TiWkwebviewConfigurationProxy {
+    WKWebViewConfiguration *configuration;
+}
+
+- (WKWebViewConfiguration *)configuration
+{
+    WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+    
+    id processPool = [self valueForKey:@"processPool"];
+    
+    if ([self valueForKey:@"processPool"]) {
+        ENSURE_TYPE(processPool, TiWkwebviewProcessPoolProxy);
+        [config setProcessPool:[(TiWkwebviewProcessPoolProxy *)processPool pool]];
+    }
+    
+    return config;
+}
+
+- (void)setPreferences:(id)args
+{
+    WKPreferences *prefs = [WKPreferences new];
+    
+    id minimumFontSize = [args valueForKey:@"minimumFontSize"];
+    id javaScriptEnabled = [args valueForKey:@"javaScriptEnabled"];
+    id javaScriptCanOpenWindowsAutomatically = [args valueForKey:@"javaScriptCanOpenWindowsAutomatically"];
+    
+    [prefs setMinimumFontSize:[TiUtils floatValue:minimumFontSize def:0]];
+    [prefs setJavaScriptEnabled:[TiUtils boolValue:javaScriptEnabled def:YES]];
+    [prefs setJavaScriptCanOpenWindowsAutomatically:[TiUtils boolValue:javaScriptCanOpenWindowsAutomatically def:NO]];
+    
+    [[self configuration] setPreferences:prefs];
+}
+
+- (void)setSelectionGranularity:(id)value
+{
+    ENSURE_TYPE(value, NSNumber);
+    
+    [[self configuration] setSelectionGranularity:[TiUtils intValue:value def:WKSelectionGranularityDynamic]];
+}
+
+- (void)setMediaTypesRequiringUserActionForPlayback:(id)value
+{
+    ENSURE_TYPE(value, NSNumber);
+    
+    [[self configuration] setMediaTypesRequiringUserActionForPlayback:[TiUtils intValue:value def:WKAudiovisualMediaTypeNone]];
+}
+
+- (void)setSuppressesIncrementalRendering:(id)value
+{
+    ENSURE_TYPE(value, NSNumber);
+    
+    [[self configuration] setSuppressesIncrementalRendering:[TiUtils boolValue:value]];
+}
+
+- (void)setAllowsInlineMediaPlayback:(id)value
+{
+    ENSURE_TYPE(value, NSNumber);
+    
+    [[self configuration] setAllowsInlineMediaPlayback:[TiUtils boolValue:value]];
+}
+
+- (void)setAllowsAirPlayMediaPlayback:(id)value
+{
+    ENSURE_TYPE(value, NSNumber);
+    
+    [[self configuration] setAllowsAirPlayForMediaPlayback:[TiUtils boolValue:value]];
+}
+
+- (void)setAllowsPictureInPictureMediaPlayback:(id)value
+{
+    ENSURE_TYPE(value, NSNumber);
+    
+    [[self configuration] setAllowsPictureInPictureMediaPlayback:[TiUtils boolValue:value]];
+}
+
+@end
